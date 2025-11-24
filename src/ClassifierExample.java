7d258c53aedad6bcb7409452549d9d41a91d9eb6
AshleyTr-05 <troitran13@gmail.com>
2025-11-24 14:17:42 +0700

    Sketch Classifier Class

---
+++ src/ClassifierExample.java
@@ -0,0 +1,31 @@
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+import weka.classifiers.trees.J48;
+import weka.classifiers.Evaluation;
+
+import java.util.Random;
+
+public class ClassifierExample {
+
+    public static void main(String[] args) throws Exception {
+
+        // 1. Load cleaned ARFF
+        DataSource source = new DataSource("datasets/heart_disease_preprocessed.arff");
+        Instances data = source.getDataSet();
+
+        // 2. Set class attribute
+        data.setClassIndex(data.numAttributes() - 1);
+
+        // 3. Train a J48 tree
+        J48 tree = new J48();
+        tree.buildClassifier(data);
+
+        // 4. Evaluate with 10-fold cross-validation
+        Evaluation eval = new Evaluation(data);
+        eval.crossValidateModel(tree, data, 10, new Random(1));
+
+        // 5. Output results
+        System.out.println(eval.toSummaryString());
+        System.out.println(eval.toMatrixString());
+    }
+}
